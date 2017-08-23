985ff6fce81e70ca2978950b9be0ab2ad9e768f2
Egor Ovechkin <egorka1391@gmail.com>
2017-08-23 08:44:34 +0500

    refactoring: use async function

--- index.js
+++ index.js
@@ -1,89 +1,61 @@
 const needle = require('needle');
 const cheerio = require('cheerio');
 const fs = require('fs');
+const {promisify} = require('util');
 
 const URL = 'http://www.koleso-razmer.ru/';
+const getAsync = promisify(needle.get);
 
-function parse() {
-  //Get brands
-  needle.get(URL, function(err, res) {
-    if (err) throw err;
-    if (res.statusCode !== 200) {
-      console.log(`${res.statusCode}:${res.statusMessage}`);
-      return false;
-    };
-    let $ = cheerio.load(res.body);
-    const brands = $('table.cols5 li a')
+function delayHelper(ms) {
+  const d = Date.now() + ms;
+  while (Date.now() < d) {};
+};
+
+async function getBrands() {
+  const res = await getAsync(URL);
+  if (res.statusCode !== 200) {
+    console.log(`${res.statusCode}:${res.statusMessage}`);
+    return []
+  }
+  const $ = cheerio.load(res.body);
+  const brands = $('table.cols5 li a')
     .map(function() {
       return {
         brand: $(this).text(),
         href: $(this).attr('href')
       }
     }).get();
-    console.log(brands);
-    const stream = fs.createWriteStream("./results/brands.txt");
-    stream.once('open', () => {
-      stream.write(JSON.stringify(brands));
-    })
-    //Get models
-    brands.forEach((brand) => {
-      setTimeout(() => {
-        needle.get(URL + brand.href.slice(1), function(err, res) {
-          if (err) throw err;
-          if (res.statusCode !== 200) {
-            console.log(`${res.statusCode}:${res.statusMessage}`);
-            return false;
-          };
-          $ = cheerio.load(res.body);    
-          brand.models = $('table li a')
-          .map(function() {
-            return {
-              model: $(this).text(),
-              href: $(this).attr('href')
-            }
-          }).get();
-          console.log(brand.models);
-          const bAndM = fs.createWriteStream("./results/bAndM.txt");
-          bAndM.once('open', () => {
-            bAndM.write(JSON.stringify(brands));
-          })
-          const modelStream = fs.createWriteStream(`./results/${brand.brand}.txt`);
-          modelStream.once('open', () => {
-            modelStream.write(JSON.stringify(brand.models));
-          })
-        })
-      }, Math.random() * 10)
-    });
-    //Get sizes
-    brands.forEach((brand) => {
-      fs.mkdir(`./results/${brand.brand}`, function() {
-        brand.models.forEach((model) => {
-          model.versions = [];
-          let currentIndex = 0;
-          setTimeout(() => {
-            needle.get(URL + model.href.slice(1), function(err, res) {
-              if (err) throw err;
-              if (res.statusCode !== 200) {
-                console.log(`${res.statusCode}:${res.statusMessage}`);
-                return false;
-              };
-              $ = cheerio.load(res.body);
-              $('tbody')
-            });
-          }, Math.random() * 10)
-          const sizesStream = fs
-          .createWriteStream(`./results/${brand.brand}/${model.model}.txt`);
-        })
-      });
-    })
-    stream.close();
-    bAndM.close();
-    modelStream.close();
-    // console.log(res.body);
-    // console.log(res.statusCode);
-  });
+  return brands;
 };
 
-module.exports = {
-  parse
-}
+async function getModels(brands) {
+  return brands.map(async function(brand) {
+    delayHelper(5 + Math.random() * 10);
+    console.log(Date(), ' - send request')
+    const res = await getAsync(URL + brand.href.slice(1));
+    if (res.statusCode !== 200) {
+      console.log(`${res.statusCode}:${res.statusMessage}`);
+      return Object.assign({}, brand, {models: []});
+    };
+    $ = cheerio.load(res.body);    
+    const models = $('table li a')
+    .map(function() {
+      return {
+        model: $(this).text(),
+        href: $(this).attr('href')
+      }
+    }).get();
+    return Object.assign({}, brand, {models});
+  })
+};
+
+// async function saveSizes(brandsWithModels) {
+
+// };
+
+(async function init() {
+  const brands = await getBrands();
+  console.log(brands)
+  const brandsWithModels = await Promise.all(await getModels(brands));
+  console.log(brandsWithModels);
+})();
